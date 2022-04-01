27ae1fffc5cdd3573632d7ed9441fff5250439a9
Charles Peace <charlespeace@hotmail.com>
2022-04-01 15:17:13 -0700

    load locally stored data, test and debug functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(function() {
+
     // Function to check time
     function checkTime() {
         var currentTime = moment().hours();
@@ -26,13 +27,26 @@ $(function() {
     // Listen and save notes to local storage
     $('.saveBtn').on('click', function() {
         var note = $(this)
-            .siblings('description')
+            .siblings('.description')
+            .val()
         var time = $(this)
             .parent()
             .attr('id')
             localStorage.setItem(time, note)
-            console.log('Saved to local storage')
+            console.log('Added to local storage')
     })
+    
+    // Load locally stored data for each hour
+    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+    $('#hour-18 .description').val(localStorage.getItem('hour-18'));
+    $('#hour-19 .description').val(localStorage.getItem('hour-19'));
 
     checkTime();
 })
